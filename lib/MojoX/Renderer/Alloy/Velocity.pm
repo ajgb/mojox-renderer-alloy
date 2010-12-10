e4b6daa85311de4552cf58a1f6d2c5fcd47dc7c8
Alex J. G. Burzyński <ajgb@cpan.org>
2010-12-10 21:45:26 +0000

    passing tests for Velocity

--- lib/MojoX/Renderer/Alloy/Velocity.pm
+++ lib/MojoX/Renderer/Alloy/Velocity.pm
@@ -8,10 +8,32 @@ use Template::Alloy qw( Velocity );
 
 __PACKAGE__->attr('alloy');
 
-sub _config {
+sub _init {
     my ($self, %args) = @_;
 
-    $self->alloy( Template::Alloy->new() );
+    my $app = delete $args{app} || delete $args{mojo};
+
+    my $inc_path  = defined $app && $app->home->rel_dir('templates');
+
+    my %config = (
+        (
+            $inc_path ?
+            (
+                INCLUDE_PATH => $inc_path
+            ) : ()
+        ),
+        UNICODE     => 1,
+        ENCODING    => 'utf-8',
+        RELATIVE    => 1,
+        ABSOLUTE    => 1,
+        %{ $args{template_options} || {} },
+    );
+
+    my $alloy = Template::Alloy->new(
+        %config,
+    );
+
+    $self->alloy( $alloy );
 }
 
 sub _render {
@@ -22,16 +44,23 @@ sub _render {
 
     my $alloy = $self->alloy;
 
-    $$output = $alloy->merge( $input,
+    # Template::Alloy won't handle undefined strings
+    $$output = '' unless defined $$output;
+    $alloy->merge( $input,
         {
             %{ $c->stash },
             c => $c,
         },
+        $output,
     ) || do {
         my $e = $alloy->error;
         chomp $e;
         $c->render_exception( $e );
+
+        return;
     };
+
+    return 1;
 }
 
 1;
